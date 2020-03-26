08e70804206cc73c0700d051961eb11ab757ae06
ZaLeep <lexa35695@gmail.com>
2020-03-26 19:03:53 +0200

    Update List_Functoin.cpp

--- Laba-3/List_Functoin.cpp
+++ Laba-3/List_Functoin.cpp
@@ -43,6 +43,7 @@ void List::Print() {
     }
     cout << endl;
 }
+
 bool List::isEmpty() {
     if (head == NULL) {
         return 1;
@@ -56,4 +57,42 @@ List::nodePtr List::LastElement() {
         curr = curr->next;
     }
     return curr;
+}
+
+Hash::Hash(int Size) {
+    size = Size;
+    A = new List[size];
+}
+
+void Hash::AddElement(string NewWord, string NewMean) {
+    int key = HashKey(NewWord);
+    A[key].AddNode(NewWord, NewMean);
+    score++;
+}
+
+int Hash::HashKey(string word) {
+    int key = 0;
+    for (int i = 0; i < word.size(); i++) {
+        key += (int)word[i];
+    }
+    key = key % size;
+    return key;
+}
+
+void Hash::NewTabl() {
+    size *= 2;
+
+    List* B = new List[size];
+
+    for (int i = 0; i < size / 2; i++) {
+
+    }
+}
+
+void Hash::PrintTabl() {
+    for (int i = 0; i < size; i++) {
+        cout << "Ячейка №" << i + 1 << ": ";
+        A[i].Print();
+        cout << endl;
+    }
 }
