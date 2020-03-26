75210241f26311397e39cd99c5483615cd6180cc
ZaLeep <lexa35695@gmail.com>
2020-03-26 17:24:20 +0200

    Update Laba-3.cpp

--- Laba-3/Laba-3.cpp
+++ Laba-3/Laba-3.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
-    
+    setlocale(LC_ALL, "rus");
+    Hash Slovarik(2);
+    string word, mean;
+    cin >> word;
+    cin >> mean;
+    Slovarik.AddElement(word, mean);
+    Slovarik.PrintTabl();
 }
