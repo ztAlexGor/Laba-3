77504fbc56e989fa520a7f820511636c2933f363
ZaLeep <lexa35695@gmail.com>
2020-03-26 19:48:32 +0200

    Update Laba-3.cpp

--- Laba-3/Laba-3.cpp
+++ Laba-3/Laba-3.cpp
@@ -1,12 +1,29 @@
 ﻿#include "Header.h"
+void ReadFile(Hash k);
 
 int main()
 {
     setlocale(LC_ALL, "rus");
     Hash Slovarik(2);
-    string word, mean;
-    cin >> word;
-    cin >> mean;
-    Slovarik.AddElement(word, mean);
+    ReadFile(Slovarik);
     Slovarik.PrintTabl();
+}
+
+void ReadFile(Hash k) {
+    ifstream cach;
+    string word, mean;
+    cach.open("test.txt");
+    while (!cach.eof()) {
+        int i = 0;
+        getline(cach, mean);
+        while (mean[i] != ';') {
+            word += mean[i];
+            i++;
+        }
+        mean.erase(0, i);
+        k.AddElement(word, mean);
+        word.clear();
+        mean.clear();
+        i = 0;
+    }
 }
