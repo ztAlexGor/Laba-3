ef207010d54381241019b1d23bfd6c846ef38039
ZaLeep <lexa35695@gmail.com>
2020-03-26 16:25:27 +0200

    Update Source.cpp

--- Laba-3/Source.cpp
+++ Laba-3/Source.cpp
@@ -1,45 +1,50 @@
 #include "Header.h"
 
 List::List() {
-	head = NULL;
-	curr = NULL;
-	temp = NULL;
+    head = NULL;
+    curr = NULL;
+    temp = NULL;
 }
 void List::AddNode(string newWord, string NewData) {
-	nodePtr n = new node;
-	n->word = newWord;
-	n->data = NewData;
-	n->next = head;
-	head = n;
+    nodePtr n = new node;
+    n->word = newWord;
+    n->data = NewData;
+    n->next = head;
+    head = n;
 }
 void List::DeleteNode(string DelWord) {
-	nodePtr delPtr = NULL;
-	curr = head;
-	temp = head;
-	while (curr != NULL && curr->data != DelWord) {
-		temp = curr;
-		curr = curr->next;
-	}
-	if (curr == NULL) {
-		cout << DelWord << " was not in the list\n";
-		delete delPtr;
-	}
-	else {
-		delPtr = curr;
-		curr = curr->next;
-		temp->next = curr;
-		if (delPtr == head) {
-			head = head->next;
-			temp = NULL;
-		}
-		delete delPtr;
-	}
+    nodePtr delPtr = NULL;
+    curr = head;
+    temp = head;
+    while (curr != NULL && curr->data != DelWord) {
+        temp = curr;
+        curr = curr->next;
+    }
+    if (curr == NULL) {
+        cout << DelWord << " was not in the list\n";
+        delete delPtr;
+    }
+    else {
+        delPtr = curr;
+        curr = curr->next;
+        temp->next = curr;
+        if (delPtr == head) {
+            head = head->next;
+            temp = NULL;
+        }
+        delete delPtr;
+    }
 }
 void List::Print() {
-	curr = head;
-	while (curr != NULL) {
-		cout << curr->word << " " << curr->data<<endl;
-		curr = curr->next;
-	}
-	cout << endl;
+    curr = head;
+    while (curr != NULL) {
+        cout << curr->word << " " << curr->data << endl;
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+Hash::Hash(int Size) {
+    size = Size;
+    A = new List[size];
 }
