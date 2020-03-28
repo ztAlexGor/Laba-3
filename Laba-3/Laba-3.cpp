8de630a24baa05e9ce1a6e2d18094d341fe09610
ZaLeep <lexa35695@gmail.com>
2020-03-28 14:23:51 +0200

    Update Laba-3.cpp

--- Laba-3/Laba-3.cpp
+++ Laba-3/Laba-3.cpp
@@ -5,7 +5,7 @@ void Link(Hash *k);
 int main()
 {
     setlocale(LC_ALL, "rus");
-    Hash Slovarik(1000);
+    Hash Slovarik(32000);
     ReadFile(&Slovarik);
     //Slovarik.PrintTabl();
     Link(&Slovarik);
@@ -13,45 +13,51 @@ int main()
 }
 
 void ReadFile(Hash *k) {
-    ifstream cach;
-    string word, mean;
-    cach.open("D:\\Учёба\\Файлы общего доступа\\boom.txt");
-    while (!cach.eof()) {
-        int i = 0;
-        getline(cach, mean);
-        while (mean[i] != ';') {
-            word += mean[i];
-            i++;
+        ifstream cach;
+        string word, mean;
+        cach.open("D:\\Учёба\\Файлы общего доступа\\boom.txt");
+        while (!cach.eof()) {
+            int i = 0;
+            getline(cach, mean);
+            while (mean[i] != ';') {
+                word += mean[i];
+                i++;
+            }
+            mean.erase(0, i + 1);
+            (*k).AddElement(word, mean);
+            word.clear();
+            mean.clear();
+            i = 0;
         }
-        mean.erase(0, i + 1);
-        (*k).AddElement(word, mean);
-        word.clear();
-        mean.clear();
-        i = 0;
-    }
-    cach.close();
+        cach.close();
 }
 
 void Link(Hash *k) {
-    cout << "Enter sentence to get definition of words:" << endl;
-    
-    string sentence, word;
+    bool z = true;
+    while (z) {
+        cout << "Enter sentence to get definition of words:" << endl;
 
-    getline (cin, sentence);
-    sentence += '.';
-    cout << endl;
-    
-    for (int i = 0; i < sentence.size(); i++) {
-        if (sentence[i] != ' ' && sentence[i] != ',' && sentence[i] != ':' && sentence[i] != '.') {
-            if ((int)sentence[i] >= 97 && (int)sentence[i] <= 122)
-                sentence[i] = (char)((int)sentence[i] - 32);
-            word += sentence[i];
-        }
-        else
-            if (!word.empty()) {
-                (*k).Search(word);
-                word.clear();
-                cout << endl;
+        string sentence, word;
+
+        getline(cin, sentence);
+        sentence += '.';
+        cout << endl;
+
+        for (int i = 0; i < sentence.size(); i++) {
+            if (sentence[i] != ' ' && sentence[i] != ',' && sentence[i] != ':' && sentence[i] != '.') {
+                if ((int)sentence[i] >= 97 && (int)sentence[i] <= 122)
+                    sentence[i] = (char)((int)sentence[i] - 32);
+                word += sentence[i];
             }
+            else
+                if (!word.empty()) {
+                    (*k).Search(word);
+                    word.clear();
+                    cout << endl;
+                }
+        }
+        cout << "Enter \"0\" to exit." << endl << "Enter \"1\" to enter one more sentence.";
+        z = (bool)_getch();
+        system("cls");
     }
 }
